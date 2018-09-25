e069071ed2ffb0cb9c4bc8fec76111e79f86c238
Daland Dixon <dalanddixon@Dalands-MacBook-Pro.local>
2018-09-25 18:42:57 -0400

    Trying again

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,104 +1,109 @@
 $(document).ready(function () {
 
-    // var queryurl = "https://beermapping.com/webservice/loccity/48cae02b055a1f73e25b8d2c344bff76/philadelphia" + "&s=json "
-    // // var locquery;
-    // // var loccity;
-    // // var loc
-
-    // $.ajax({
-    //     url: queryurl,
-    //     method: "Get"
-    // }).then(function (response) {
-    //     console.log(response[0]);
-
-    //     $("#bar-name-display").text(response[0].name);
-    //     $("#bar-address-display").text("Address: " + response[0].street);
-    //     $("#bar-rating-display").text("Rating: " + response[0].overall);
-    //     var map = $("<iframe class='embed-responsive-item' id='map'>");
-    //     $(map).attr("src", response[0].proxylink);
-    //     // $(map).attr("height", "650px").attr("width", "523px");
-    //     $(".mapHere").empty().append(map);
-    // });
-
-    // $.ajax({
-    //     url: queryurl,
-    //     method: "Get"
-    // }).then(function (response) {
-    //     console.log(response[0]);
-
-    // $("#submit").on("click", function () {
-
-    //     var location = $("#inputLocation").val()
-    //     console.log("This is the location: " + location)
-    //     var queryurl = "http://beermapping.com/webservice/loccity/48cae02b055a1f73e25b8d2c344bff76/" + location + "&s=json "
-
-    //     $.ajax({
-    //         url: queryurl,
-    //         method: "Get"
-    //     }).then(function (response) {
-    //         console.log(response);
-    //     });
-    // });
-
-
-
+    // ==========================================
 
     // AIzaSyAYaTv-hKyNAp7NAQ1mKdP_F5-vpkfouoU
 
+    // ==========================================
 
+    var destination;
+    var placeID;
 
-    $("#submit").on("click", function () {
-        console.log($("#inputLocation").val())
+    $("#directions-btn").on("click", function () {
+        window.open("https://www.google.com/maps/dir/?api=1&destination=" + destination + "+Philadelphia+PA" + "&destination_place_id=" + placeID);
     });
 
-    // TEXT SEARCH =====
+    $(".splashPage").show()
+    $(".locationPage").show()
+    $("#displayPage").hide()
 
-    var searchQuery = "query=" + $("#inputLocation").val().trim() +", philadelphia"
-    var key = "&key=AIzaSyAYaTv-hKyNAp7NAQ1mKdP_F5-vpkfouoU"
-    var proxyURL = "https://cors-anywhere.herokuapp.com/"
-    var targetURL1 = "https://maps.googleapis.com/maps/api/place/textsearch/json?" + searchQuery + key
-    var queryURL1 = proxyURL + targetURL1
+    $("#loc-submit").on("click", function () {
+        console.log("hello")
+        console.log($("#inputLocation").val().trim())
 
-    $.ajax({
-        url: queryURL1,
-        method: "Get",
-    }).then(function (response) {
+        $(".splashPage").hide()
+        $(".locationPage").hide()
+        $("#displayPage").show()
 
-        console.log(response.results[0].formatted_address);
-        console.log(response.results[0].geometry.location.lat);
-        console.log(response.results[0].geometry.location.lng);
+        // ============ TEXT SEARCH =================
+        // finds the latitude and longitude of the location
+        // entered into the search bar
 
-        // NEARBY SEARCH (Requires lat and long)
-
-        var lat = response.results[0].geometry.location.lat;
-        var lng = response.results[0].geometry.location.lng;
-        var location = "location=" + lat + "," + lng
-        var targetURL2 = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?" + location + "&radius=800&type=bar" + key;
-        var queryURL2 = proxyURL + targetURL2;
+        var location = $("#inputLocation").val().trim()
+        // var location = "south philadelphia"
+        var searchQuery = "query=" + location + ", philadelphia"
+        var key = "&key=AIzaSyAYaTv-hKyNAp7NAQ1mKdP_F5-vpkfouoU"
+        var proxyURL = "https://vast-shelf-62764.herokuapp.com/"
+        var targetURL1 = "https://maps.googleapis.com/maps/api/place/textsearch/json?" + searchQuery + key
+        var queryURL1 = proxyURL + targetURL1
 
         $.ajax({
-            url: queryURL2,
+            url: queryURL1,
             method: "Get",
-        }).then(function (response2) {
+        }).then(function (response) {
 
-            console.log(targetURL2)
+            console.log(response.results[0].formatted_address);
 
-            for (var i = 0; i < response2.results.length; i++) {
-                console.log(response2.results[i].name)
-                // console.log(response2.results[i].rating)
-                // console.log(response2.results[i].vicinity)
-                // console.log(response2.results[i].geometry.location)
-            }
-            console.log(response2.results)
+            // ================= NEARBY SEARCH ================
+            // uses the latitude and longitude to find local bars
+            // and display them on the page
 
+            var lat = response.results[0].geometry.location.lat;
+            var lng = response.results[0].geometry.location.lng;
+            var location = "location=" + lat + "," + lng
+            var targetURL2 = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?" + location + "&radius=800&type=bar" + key;
+            var queryURL2 = proxyURL + targetURL2;
+
+            $.ajax({
+                url: queryURL2,
+                method: "Get",
+            }).then(function (response2) {
+
+                console.log(response2.results)
+
+                function loop() {
+                    var listItemDiv = $("<div>");
+                    listItemDiv.attr("class", "col-4 text-center p-0 list-item-div bar-btn");
+                    listItemDiv.attr("id", "li" + i);
+                    listItemDiv.attr("name", response2.results[i].name);
+                    listItemDiv.attr("rating", response2.results[i].rating);
+                    listItemDiv.attr("addr", response2.results[i].vicinity.split(", Phi").shift());
+                    listItemDiv.attr("place-id", response2.results[i].place_id);
+                    var listItem = $("<p>");
+                    $("#mapHere").append(listItemDiv);
+                    $(listItemDiv).append(listItem);
+                    var str = response2.results[i].name + "<br>" + response2.results[i].vicinity.split(", Phi").shift();
+                    $(listItem).html(str);
+                };
+
+                if (response2.results.length >= 10) {
+
+                    for (var i = 0; i < 10; i++) {
+                        loop();
+                    }
+                } else {
+                    for (var i = 0; i < response2.results.length; i++) {
+                        loop();
+                    };
+                }
+
+                $(document.body).on("click", ".bar-btn", function () {
+
+                    $("#bar-name").text($(this).attr("name"));
+                    $("#bar-rating").text("Rating: " + $(this).attr("rating"));
+                    $("#bar-addr").text($(this).attr("addr"));
+                    destination = $(this).attr("addr");
+                    placeID = $(this).attr("place-id");
+
+                });
+
+            });
+
+            // ==========================================
 
         });
 
         // ==========================================
-
     });
-
-
-    // ==========================================
 
 });
