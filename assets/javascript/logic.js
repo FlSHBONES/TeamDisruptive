7fa62726e67df8c3834095421bb5ea1eacdf8325
A-Engman <anders.p.engman@gmail.com>
2018-09-25 20:29:29 -0400

    Additions to logic file; disabled users from creating many duplicate profiles easily

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -17,6 +17,8 @@ var config = {
   var userName = "";
   var verifyPassword = "";
   var database = firebase.database();
+  var childData;
+  var userAdded = false;
   
   console.log("pass variables");
   
@@ -25,7 +27,7 @@ var config = {
     $("#submit").on("click", function() {
         console.log("firing");
   
-        userName = $("#inputEmail").val().trim();
+        userName = $("#inputUserName").val().trim();
         password = $("#inputPassword").val().trim();
   
         if (!userName || !password) {
@@ -35,71 +37,64 @@ var config = {
         console.log(userName);
         console.log(password);
   
-        database.ref("/userBase").on("value", function(snapshot) {
-          console.log(snapshot.val());
-          if (snapshot.child("userName").exists() && snapshot.child("password").exists()) {
-            if (snapshot.child("password") == password) {
-              console.log("Hell yeah man");
-              window.location.href = "location.html";
-            } else if (snapshot.child("password") != password) {
-              console.log("No Login Fer YEH");
-            }
-            // EXAMPLE QUERYING OF FIREBASE:
-            // var ratingRef = firebase.database().ref("ratings/");
-  
-            // ratingRef.orderByValue().on("value", function(data) {
-               
-            //    data.forEach(function(data) {
-            //       console.log("The " + data.key + " rating is " + data.val());
-            //    });
-               
-            // });
-          }
-        });
+        var userRef = database.ref('userBase');
+        userRef.on('value', function(snapshot) {
+          snapshot.forEach(function(childSnapshot) {
+            var childData = childSnapshot.val();
+            console.log(childData);
+            // if ((childData == userName) && ()
+          });
+        })
     });
   
     // Register User
     $("#submitReg").on("click", function() {
-  
-      password = $("#inputPasswordReg").val().trim();
-      verifyPassword = $("#inputConfirmPasswordReg").val().trim();
-      firstName = $("#inputFirstNameReg").val().trim();
-      lastName = $("#inputLastNameReg").val().trim();
-      userName = $("#inputUserNameReg").val().trim();
-      // verifyPassword = $("inputConfirmPasswordReg").val().trim();
-  
-      if (!userName || !password) {
-        return console.log('username and password required')
+
+      if (userAdded === false) {
+        password = $("#inputPasswordReg").val().trim();
+        verifyPassword = $("#inputConfirmPasswordReg").val().trim();
+        firstName = $("#inputFirstNameReg").val().trim();
+        lastName = $("#inputLastNameReg").val().trim();
+        userName = $("#inputUserNameReg").val().trim();
+        // verifyPassword = $("inputConfirmPasswordReg").val().trim();
+    
+        if (!userName || !password) {
+          return console.log('username and password required')
+        }
+    
+        console.log(password);
+        console.log(verifyPassword);
+    
+        if (password != verifyPassword) {
+          return console.log('passwords do not match')
+        }
+    
+        if ((document.getElementById("ageCheck").checked) == true) {
+          console.log('21+ age verify');
+        } else {
+          return console.log('Please Verify Age')
+        }
+    
+        console.log(userName);
+        console.log(password);
+        console.log(firstName);
+        console.log(lastName);
+        console.log(userName);
+        console.log(verifyPassword);
+    
+        database.ref("/userBase").push ({
+          password: password,
+          firstName: firstName,
+          lastName: lastName,
+          userName: userName
+        });
+
+        console.log("new user added");
+        userAdded = true;
+
+      } else if (userAdded === true) {
+        console.log("Not today mofucka")
       }
-  
-      console.log(password);
-      console.log(verifyPassword);
-  
-      if (password != verifyPassword) {
-        return console.log('passwords do not match')
-      }
-  
-      if ((document.getElementById("ageCheck").checked) == true) {
-        console.log('21+ age verify');
-      } else {
-        return console.log('Please Verify Age')
-      }
-  
-      console.log(userName);
-      console.log(password);
-      console.log(firstName);
-      console.log(lastName);
-      console.log(userName);
-      console.log(verifyPassword);
-  
-      database.ref("/userBase").push ({
-        password: password,
-        firstName: firstName,
-        lastName: lastName,
-        userName: userName
-      });
-  
-      console.log("user added");
   
     });
   });
